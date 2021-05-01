a69a4a0201027b49e8561f6b772d7dec820cf839
Ethereum Developer <ethereumdeveloper001@gmail.com>
2021-05-01 17:55:15 +0100

    Genesis minting

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 import { contract, wallet } from './common.js';
 
 async function updateProgress() {
-  let supply = parseInt(await contract.methods.getGenerativeSupply().call());
-  let minted = parseInt(await contract.methods.getGenerativeMinted().call());
+  var supply = parseInt(await contract.methods.getGenerativeSupply().call());
+  var minted = parseInt(await contract.methods.getGenerativeMinted().call());
   let generative = minted.toLocaleString().concat(" / ").concat(supply.toLocaleString());
   document.querySelector('#progress-generative').innerHTML = generative.concat(" CLAIMED");
+
+  supply = parseInt(await contract.methods.getGenesisSupply().call());
+  minted = parseInt(await contract.methods.getGenesisMinted().call());
+  let genesis = minted.toLocaleString().concat(" / ").concat(supply.toLocaleString());
+  document.querySelector('#progress-genesis').innerHTML = genesis.concat(" CLAIMED");
 }
 
 document.onload = updateProgress();
