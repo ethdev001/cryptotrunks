2c7c7fab26dff9220977c869e2b920566de8ce89
Ethereum Developer <ethereumdeveloper001@gmail.com>
2021-05-04 20:32:09 +0100

    Gen export

--- js/genesis.js
+++ js/genesis.js
@@ -3,8 +3,11 @@ import { contract, wallet } from './common.js';
 async function updateTotals() {
   let minted = parseInt(await contract.methods.getGenesisMinted().call());
   let supply = parseInt(await contract.methods.getGenesisSupply().call());
-  document.querySelector('#genesis-claimed').innerHTML = minted.toLocaleString();
-  document.querySelector('#genesis-remaining').innerHTML = (supply - minted).toLocaleString();
+
+  let trunks = (minted == 1 ? "trunk has" : "trunks have");
+  let stats = `Right now <strong>${minted.toLocaleString()}</strong> genesis ${trunks} been claimed. There are only <strong>${(supply - minted).toLocaleString()}</strong> genesis trunks remaining!<br>`
+
+  document.querySelector('#genesis-stats').innerHTML = stats;
 }
 
 document.onload = updateTotals();
