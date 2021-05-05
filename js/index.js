0352e42561177807bdf9fdf8f0df42d986a28ee4
Ethereum Developer <ethereumdeveloper001@gmail.com>
2021-05-05 22:06:45 +0100

    Progress bars

--- js/index.js
+++ js/index.js
@@ -5,11 +5,15 @@ async function updateProgress() {
   var minted = parseInt(await contract.methods.getGenerativeMinted().call());
   let generative = minted.toLocaleString().concat(" / ").concat(supply.toLocaleString());
   document.querySelector('#progress-generative').innerHTML = generative.concat(" CLAIMED");
+  var progress = parseInt((parseFloat(minted) / parseFloat(supply)) * 100);
+  document.querySelector('#progress-bar-generative').style = `background-size: contain, contain, ${progress}% 90%, contain;`;
 
   supply = 1500;
   minted = parseInt(await contract.methods.getGenesisMinted().call());
   let genesis = minted.toLocaleString().concat(" / ").concat(supply.toLocaleString());
   document.querySelector('#progress-genesis').innerHTML = genesis.concat(" CLAIMED");
+  var progress = parseInt((parseFloat(minted) / parseFloat(supply)) * 100);
+  document.querySelector('#progress-bar-genesis').style = `background-size: contain, contain, ${progress}% 90%, contain;`;
 }
 
 document.onload = updateProgress();
