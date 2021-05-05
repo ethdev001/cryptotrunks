8dcb2a17e3dd8c8c4861ae77c26f9f07aef531c9
Ethereum Developer <ethereumdeveloper001@gmail.com>
2021-05-05 21:28:58 +0100

    Re-enable wallet stuff

--- js/wallet.js
+++ js/wallet.js
@@ -15,6 +15,35 @@ async function loadWalletTrunks() {
     document.querySelector('#token-count').innerHTML = `You own <strong id="token-count">${tokens}</strong> ${trunks}.<br>`;
   });
 
+  var grid = "";
+  if (tokens == 0) {
+    grid = `<div class="text_block">
+      <a href="generate-in-progress.html" class="button_two w-inline-block">
+        <div class="button_text">GET SOME TRUNKS</div>
+      </a>
+    </div>`;
+  } else {
+    grid = grid.concat('<div class="wallet_grid">');
+    for (let i = 0; i < Math.min(tokens, 18); i++) {
+      let token = await contract.methods.tokenOfOwnerByIndex(wallet, i).call();
+      let uri = await contract.methods.tokenURI(token).call();
+      let metadata = await (await fetch(uri)).json();
+
+      if (metadata.error == undefined) {
+        grid = grid.concat(`
+          <a href="individual-trunk-page.html?token=${metadata.id}" class="gallery_item w-inline-block">
+            <img src="${metadata.image}" loading="lazy" alt="Image of a pixel art tree on a forested background." class="wallet_item_image">
+            <div class="text_block wallet_info" style="min-width: 0px">
+              <div class="info_text big">TRUNK #${metadata.number}<br></div>
+              <div class="info_text">${formattedResult(metadata)}</div>
+            </div>
+          </a>`);
+      }
+    }
+    grid = grid.concat('</div>');
+  }
+  document.querySelector('#wallet-grid').innerHTML = grid;
+
   document.querySelector('#wallet-opensea').href = `https://opensea.io/accounts/${wallet}/CryptoTrunks`;
 }
 
