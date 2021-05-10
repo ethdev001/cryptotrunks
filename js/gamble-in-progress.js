2218ba325343c4f7736a03e59faf4298ddf88b69
Ethereum Developer <ethereumdeveloper001@gmail.com>
2021-05-10 17:39:00 +0100

    Horse modal for gamble

--- js/gamble-in-progress.js
+++ js/gamble-in-progress.js
@@ -1,11 +1,13 @@
 import { web3, wallet, contract } from './common.js';
 
 async function ape() {
+  document.querySelector('#loading-text').innerHTML = "CONNECTING...";
+  document.querySelector('#loading-modal').style = "display:flex";
+
   contract.events.RemoteMintTwentyFulfilled({}, function(error, result) {
     if (!error) {
       let resultId = result.returnValues.resultId;
       console.log("Minted: " + resultId);
-      document.result = result;
       window.location.href = `wallet.html`;
     } else {
       console.log("Mint error: " + error.message);
@@ -13,11 +15,14 @@ async function ape() {
   });
 
   let fee = web3.utils.toWei("1");
-  let mint = await contract.methods.mintTwentyTrunks().send({ from: wallet, value: fee }).catch (function (error){
+  let mint = await contract.methods.mintTwentyTrunks()
+    .send({ from: wallet, value: fee })
+    .then(function(result) {
+      document.querySelector('#loading-text').innerHTML = `GROWING TRUNKS...`;
+    })
+    .catch (function (error){
       window.location.href = "generate.html";
   });
-
-  // window.location.href = "wallet.html";
 }
 
 document.onload = ape();
