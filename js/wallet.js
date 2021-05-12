db76e012f3479cf2e7a02d06f12c154a1bde8c3d
Ethereum Developer <ethereumdeveloper001@gmail.com>
2021-05-12 22:26:51 +0100

    Fix up numbering

--- js/wallet.js
+++ js/wallet.js
@@ -38,7 +38,7 @@ async function loadWalletTrunks() {
           <a href="individual-trunk-page.html?token=${metadata.id}" class="gallery_item w-inline-block">
             <img src="${metadata.image}" loading="lazy" alt="Image of a pixel art tree on a forested background." class="wallet_item_image">
             <div class="text_block wallet_info" style="min-width: 0px">
-              <div class="info_text big">TRUNK #${metadata.number}<br></div>
+              <div class="info_text big">TRUNK #${token}<br></div>
               <div class="info_text">${formattedResult(metadata)}</div>
             </div>
           </a>`);
