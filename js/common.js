78b3d55df81a1e87e81522adeb6783e39b328c10
Ethereum Developer <ethereumdeveloper001@gmail.com>
2021-05-21 13:50:02 +0100

    Support fallback for Coinbase Wallet

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ import { abi, address } from './contract.js';
 export let web3 = new Web3(ethereum);
 let accounts = await web3.eth.getAccounts();
 export let contract = new web3.eth.Contract(abi, address);
-export let wallet = ethereum.selectedAddress;
+export let wallet = ethereum.selectedAddress || accounts[0];
 
 const isMetaMaskConnected = async () => {
     let accounts = await web3.eth.getAccounts();
